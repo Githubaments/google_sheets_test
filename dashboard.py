4e6cb9092d74f2604e757ff8d2c5f44ff975700d
Githubaments <flynnshane@gmail.com>
2023-03-15 22:26:00 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,26 @@
 import streamlit as st
+from google.oauth2 import service_account
 from gsheetsdb import connect
 
-# Connect to Google Sheets
-conn = connect()
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
-# Query the specific sheet by name
-query = "SELECT * FROM `Form responses 2$`"
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Execute the query and read the results into a pandas dataframe
 df = conn.execute(query).fetch_data()
